ac6e2e23fd20ff509ff226e7e59f3002deb38902
Chris Ferguson <Christopher.Ferguson1@edu.sait.ca>
2024-10-13 15:24:35 -0600

    Added schema

--- Web Development/Node JS/Informational Website EJS/utils/database.js
+++ Web Development/Node JS/Informational Website EJS/utils/database.js
@@ -1,6 +1,18 @@
 // Chris Ferguson - Node JS assignment
 
 // Connects to database using sequelize
+// CREATE SCHEMA `informational_website` ;
+// CREATE TABLE `users` (
+//   `id` int NOT NULL AUTO_INCREMENT,
+//   `firstname` varchar(45) NOT NULL,
+//   `lastname` varchar(45) NOT NULL,
+//   `email` varchar(45) NOT NULL,
+//   `phone` int NOT NULL,
+//   `city` varchar(45) NOT NULL,
+//   `postal` varchar(6) NOT NULL,
+//   `message` varchar(240) NOT NULL,
+//   PRIMARY KEY (`id`)
+// ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 const Sequelize = require('sequelize');
 
